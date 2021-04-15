c55904949a0ed6d052ffe4ad01e98fc8acb69186
Julien Descannevelle <jdesca.eirl@gmail.com>
2021-04-15 09:59:02 +0200

    commit

--- View/listPizza.php
+++ View/listPizza.php
@@ -5,22 +5,7 @@
         <ul>
         <?php foreach($_SESSION['listPizza'] as $pizza) : ?>
             <li>
-
-            <?php 
-                $isAllergen = false;
-                foreach($pizza->getIngredients() as $ingredient){
-                    if($ingredient->getIsAllergen()){
-                        $isAllergen = true;
-                        break;
-                    }
-                }
-                ?>
-
-                <?php if($isAllergen) : ?>
-                    <a href="/detailPizza?id=<?=$pizza->getId()?>"><strong><?= $pizza->getName() ?></strong></a>
-                <?php else: ?>
-                    <a href="/detailPizza?id=<?=$pizza->getId()?>"><?= $pizza->getName() ?></a>
-                <?php endif ?>
+                <a href="/detailPizza?id=<?=$pizza->getId()?>"><?= $pizza->getName() ?></a>
                 <a href="/deletePizza?id=<?=$pizza->getId()?>">Supprimer</a>
             </li>
         <?php endforeach ?>
